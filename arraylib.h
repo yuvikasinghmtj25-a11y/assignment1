0c59164fc48b595add170a676834e7e028f7e0e2
yuvikasinghmtj25-a11y <yuvikasingh.mtj25@gmail.com>
2025-10-20 12:39:12 +0530

    Add files via upload

---
+++ arraylib.h
@@ -0,0 +1,10 @@
+#ifndef ARRAYLIB_H
+#define ARRAYLIB_H
+int findMaxIndex(int arr[],int size);
+int findMinIndex(int arr[],int size);
+float findAverage(int arr[],int size);
+void displayArray(int arr[],int size);
+void reverseArray(int arr[],int size);
+void sortArray(int arr[],int size);
+int linearSearch(int arr[],int size,int value);
+#endif
